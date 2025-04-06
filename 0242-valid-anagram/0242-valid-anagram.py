551d2e7ded68dd2ad0654d0ced19621cbc7c0fe3
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-06 16:56:40 -0700

    Time: 22 ms (59.13%), Space: 13.4 MB (39.88%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        s = sorted(s)
+        t = sorted(t)
+        return s == t
+
+        
