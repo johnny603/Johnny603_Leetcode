bf98c20a091eab9a802cd1b95068a9fa3aa0c1fe
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-12 18:22:59 -0700

    Time: 1 ms (99.1%), Space: 42 MB (79.79%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false; // contains invalid character
+            }
+
+            if (Character.isLetter(c)) {
+                if ("aeiouAEIOU".indexOf(c) != -1) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
