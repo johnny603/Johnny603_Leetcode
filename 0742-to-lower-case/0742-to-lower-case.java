5a43b22ff3d8e16afb63642a4c0aa611aa462c8d
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-12 18:36:22 -0700

    Time: 1 ms (21.55%), Space: 41.7 MB (59.79%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else {
+                result.append(c);
+            }
+        }
+        
+        return result.toString();
+    }
+}
