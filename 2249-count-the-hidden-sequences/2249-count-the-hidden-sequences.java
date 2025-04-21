2c7de053995faa5610d8225cee3ce31913ce5dec
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-20 17:19:07 -0700

    Time: 4 ms (92.06%), Space: 61.2 MB (84.13%) - LeetHub

---
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long sum = 0;
+        long minPrefix = 0;
+        long maxPrefix = 0;
+        
+        for (int diff : differences) {
+            sum += diff;
+            minPrefix = Math.min(minPrefix, sum);
+            maxPrefix = Math.max(maxPrefix, sum);
+        }
+        
+        long minStart = lower - minPrefix;
+        long maxStart = upper - maxPrefix;
+        
+        long count = maxStart - minStart + 1;
+        return (int)Math.max(0, count);
+    }
+}
