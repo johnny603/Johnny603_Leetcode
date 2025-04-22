ab67a730f4389eafbf4f38747dd59ac2acbeec9c
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-22 16:56:06 -0700

    Time: 19 ms (30.61%), Space: 58.3 MB (44.66%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+// idea:
+// Sort array then check for duplicates
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {  // only go to second-to-last index
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
