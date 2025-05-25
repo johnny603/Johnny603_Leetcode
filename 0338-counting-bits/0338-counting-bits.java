fb059a09ee9ee1f362ab807ac32c384e8aabda34
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-05-24 18:43:24 -0700

    Time: 3 ms (33.15%), Space: 47.1 MB (12.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,9 +3,18 @@ class Solution {
         int[] ans = new int[n + 1];
 
         for (int i = 0; i <= n; i++) {
-            ans[i] = Integer.bitCount(i);  // counts number of 1s in binary
+            ans[i] = countOnes(i);
         }
 
         return ans;
+    }
+
+    private int countOnes(int num) {
+        int count = 0;
+        while (num > 0) {
+            num = num & (num - 1);  // clear the lowest set bit
+            count++;
+        }
+        return count;
     }
 }
