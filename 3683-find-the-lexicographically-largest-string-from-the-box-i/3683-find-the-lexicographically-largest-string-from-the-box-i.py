8e061d366fc3178391cb68936a675a7cace84e14
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-06-03 19:39:00 -0700

    Time: 20 ms (31.82%), Space: 17.9 MB (64.39%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
@@ -1,13 +1,13 @@
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
-        if numFriends == 1:
+        if numFriends == 1: # base case
             return word
 
         max_substring = ''
         substring_size = len(word) - numFriends + 1
 
         for i in range(len(word)):
-            max_substring = max(max_substring, word[i:i + substring_size])
+            max_substring = max(max_substring, word[i:i + substring_size]) # get all possible splits
 
         return max_substring
 
