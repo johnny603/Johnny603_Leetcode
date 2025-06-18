7a3af33a38317d6b8950ad1f2469f1ffa8c5bd77
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-06-17 17:45:59 -0700

    Time: 20 ms (97.91%), Space: 61.3 MB (35.6%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
-        List<int[]> result = new ArrayList<>();
+        List<int[]> result = new ArrayList<>(); // array list of arrays
 
         for (int i = 0; i < nums.length; i += 3) { // n is a multiple of 3
             int[] group = new int[]{nums[i], nums[i + 1], nums[i + 2]}; // size 3 array
@@ -15,6 +15,6 @@ class Solution {
             result.add(group); // append array
         }
 
-        return result.toArray(new int[result.size()][]);
+        return result.toArray(new int[result.size()][]); // convert the array list to a 2d array
     }
 }
