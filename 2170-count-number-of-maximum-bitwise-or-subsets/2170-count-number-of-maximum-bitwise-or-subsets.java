ef50dff6132ae1d45029509bc8e6094026b1f2f6
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-07-27 17:08:49 -0700

    Time: 8 ms (80.38%), Space: 41.1 MB (72.92%) - LeetHub

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,59 @@
+/*
+function countMaxOrSubsets(nums: int[]) -> int:
+    int maxOr = 0
+    for num in nums:
+        maxOr |= num   // Find the maximum OR possible from all elements
+    
+    int count = 0
+
+    // DFS helper function
+    function dfs(index: int, currentOr: int):
+        if index == nums.length:
+            if currentOr == maxOr:
+                count += 1
+            return
+        
+        // Option 1: Include nums[index] in subset
+        dfs(index + 1, currentOr | nums[index])
+
+        // Option 2: Exclude nums[index] from subset
+        dfs(index + 1, currentOr)
+
+    // Start DFS from index 0 and initial OR as 0
+    dfs(0, 0)
+
+    return count
+*/
+
+class Solution {
+    private int maxOr = 0;
+    private int count = 0;
+
+    public int countMaxOrSubsets(int[] nums) {
+        // Step 1: Find the maximum OR value from all elements combined
+        for (int num : nums) {
+            maxOr |= num;
+        }
+
+        // Step 2: Start DFS
+        dfs(nums, 0, 0);
+
+        return count;
+    }
+
+    private void dfs(int[] nums, int index, int currentOr) {
+        // If we've considered all elements
+        if (index == nums.length) {
+            if (currentOr == maxOr) {
+                count++;
+            }
+            return;
+        }
+
+        // Include current element in the subset
+        dfs(nums, index + 1, currentOr | nums[index]);
+
+        // Exclude current element from the subset
+        dfs(nums, index + 1, currentOr);
+    }
+}
