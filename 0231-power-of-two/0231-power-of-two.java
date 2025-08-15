ccce1fa22449ea1b72a23a557a17042e3315447c
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-08-14 17:16:08 -0700

    Time: 0 ms (100%), Space: 40.8 MB (66.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,11 @@
-class Solution {
+public class Solution {
     public boolean isPowerOfTwo(int n) {
-        // A power of two in binary has exactly one '1' bit (e.g., 1=0001, 2=0010, 4=0100).
-        // Subtracting 1 flips all bits after the single '1' to the right of it (e.g., 4=0100, 3=0011).
-        // Using (n & (n - 1)) will therefore be 0 only if n is a power of two.
-        // We also check n > 0 to exclude zero and negative numbers.
-        return n > 0 && (n & (n - 1)) == 0;
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2; // divide by 2
+        }
+        return n == 1;
     }
 }
