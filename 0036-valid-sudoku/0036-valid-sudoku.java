bf8f7dad31850f3404338336862fed5ca7901c68
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-08-29 17:27:30 -0700

    Time: 2 ms (77.42%), Space: 44.5 MB (66.47%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Initialize sets to keep track of numbers in rows, columns, and boxes
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+        
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        
+        // Iterate through the board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.') {
+                    continue;
+                }
+                
+                // Check row
+                if (rows[i].contains(num)) {
+                    return false;
+                }
+                rows[i].add(num);
+                
+                // Check column
+                if (cols[j].contains(num)) {
+                    return false;
+                }
+                cols[j].add(num);
+                
+                // Check 3x3 sub-box
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if (boxes[boxIndex].contains(num)) {
+                    return false;
+                }
+                boxes[boxIndex].add(num);
+            }
+        }
+        
+        return true;
+    }
+}
