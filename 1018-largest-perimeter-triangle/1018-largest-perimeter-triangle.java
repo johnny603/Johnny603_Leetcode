79a0cb9c8d80e0e0f9fd01c9cc1382cfe3cd8390
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-09-27 19:06:56 -0700

    Time: 7 ms (99.49%), Space: 45.6 MB (5.7%) - LeetHub

---
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -0,0 +1,26 @@
+// Idea: sort array from largest to smallest
+// Then pick the first three
+// If the array is empty or has a size less than three, return 0
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        // If less than 3 sides, can't form a triangle
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+
+        // Sort ascending
+        Arrays.sort(nums);
+
+        // Start from the largest side and check triples
+        for (int i = nums.length - 1; i >= 2; i--) {
+            // Check triangle inequality: a + b > c
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+
+        // If no valid triangle
+        return 0;
+    }
+}
