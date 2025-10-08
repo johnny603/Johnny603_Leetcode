54987f3989c834b63fe6f754253bed9fccc0e344
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-10-07 18:15:51 -0700

    Time: 50 ms (17.73%), Space: 69.5 MB (5.27%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            long minPotion = (success + spells[i] - 1) / spells[i]; // ceiling division
+            int idx = binarySearch(potions, minPotion);
+            result[i] = m - idx;
+        }
+
+        return result;
+    }
+
+    private int binarySearch(int[] potions, long target) {
+        int left = 0, right = potions.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (potions[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
