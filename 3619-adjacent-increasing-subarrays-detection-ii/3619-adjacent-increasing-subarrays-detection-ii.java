d11d936c17fef09c8f6d3f9ead62e8e1a1650111
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-10-14 18:42:17 -0700

    Time: 14 ms (95.74%), Space: 89 MB (38.3%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,22 +1,11 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        // Variable to store the maximum length k of two adjacent strictly increasing subarrays
         int maxLength = 0;
-      
-        // Length of the previous strictly increasing subarray
         int previousLength = 0;
-      
-        // Length of the current strictly increasing subarray being processed
         int currentLength = 0;
-      
-        // Total number of elements in the list
         int n = nums.size();
-      
-        // Iterate through all elements in the list
         for (int i = 0; i < n; i++) {
-            // Increment current subarray length
             currentLength++;
-          
             // Check if we've reached the end of a strictly increasing subarray
             // This happens when we're at the last element OR the next element is not greater
             if (i == n - 1 || nums.get(i) >= nums.get(i + 1)) {
