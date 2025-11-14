571e0180eda8818205376cded734396f8beba247
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-11-13 19:00:19 -0800

    Time: 460 ms (18.86%), Space: 77.1 MB (18.86%) - LeetHub

---
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int[][] matrix = new int[n][n];
+
+        // For each query, increment the submatrix
+        for (int[] q : queries) {
+            int r1 = q[0], c1 = q[1];
+            int r2 = q[2], c2 = q[3];
+
+            for (int i = r1; i <= r2; i++) {
+                for (int j = c1; j <= c2; j++) {
+                    matrix[i][j]++;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
