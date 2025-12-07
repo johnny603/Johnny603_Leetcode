a9d546b2feec7ae76cee9b2d8a1707a76ca02776
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-12-06 16:30:15 -0800

    Time: 0 ms (100%), Space: 42.2 MB (12.5%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,21 @@
+/*
+class Solution {
+    public int countOdds(int low, int high) {
+        int count = 0;
+        while (low <= high) {
+            if (low % 2 == 1) { // odd
+                count++;
+            }
+            low++;
+        }
+        return count;
+    }
+}
+*/
+
+
+class Solution {
+    public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+}
