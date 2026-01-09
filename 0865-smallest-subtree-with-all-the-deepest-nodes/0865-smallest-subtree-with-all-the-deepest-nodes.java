4b9837fffd2396b33f99bf8a2c372eb660f801a3
Johnny Santamaria <johnnysantamaria603@gmail.com>
2026-01-08 16:53:27 -0800

    Time: 0 ms (100%), Space: 43.6 MB (33.06%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    private static class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        } else if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        } else {
+            // same depth → current node is LCA of deepest nodes
+            return new Result(root, left.depth + 1);
+        }
+    }
+}
