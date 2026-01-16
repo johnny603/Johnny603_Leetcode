19c9a948e98686c21263709fe62d6604b8f3de3d
Johnny Santamaria <johnnysantamaria603@gmail.com>
2026-01-16 14:51:43 -0800

    Time: 0 ms (100%), Space: 19.4 MB (9.16%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        # dp[i] = number of unique BSTs with i nodes
+        dp = [0] * (n + 1)
+
+        dp[0] = 1  # empty tree
+        dp[1] = 1
+
+        for nodes in range(2, n + 1):
+            for root in range(1, nodes + 1):
+                left = dp[root - 1]
+                right = dp[nodes - root]
+                dp[nodes] += left * right
+
+        return dp[n]
