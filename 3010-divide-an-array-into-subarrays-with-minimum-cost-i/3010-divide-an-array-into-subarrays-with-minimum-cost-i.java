1d59f1af95100952ba5b058f1312f1da5fbfceed
Johnny Santamaria <johnnysantamaria603@gmail.com>
2026-01-31 17:59:29 -0800

    Time: 1 ms (99.2%), Space: 45.4 MB (58.4%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,19 @@
+// always take nums[0] and take the smallest two
+class Solution {
+    public int minimumCost(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        // find two smallest elements after index 0
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return nums[0] + min1 + min2;
+    }
+}
